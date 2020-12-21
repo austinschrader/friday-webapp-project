9d24787886072b9c3b9592bd465374db7cb80c08
Austin Schrader <austin.jschrader@gmail.com>
2020-12-21 01:06:45 -0800

    Remove unnecessary values and fields

--- ForSale/Models/Vendor.cs
+++ ForSale/Models/Vendor.cs
@@ -5,19 +5,20 @@ namespace ForSale.Models
   public class Vendor
   {
     public string Name { get; set; }
-    public string Description { get; set; }
-    public string Contact { get; set; }
+    // public string Description { get; set; }
+    // public string Contact { get; set; }
     public int Id { get; }
     private static List<Vendor> _instances = new List<Vendor> { };
     public List<Order> Orders { get; set; }
 
-    public Vendor(string name, string description, string contact)
+    public Vendor(string name)
     {
       Name = name;
-      Description = description;
-      Contact = contact;
+      // Description = description;
+      // Contact = contact;
       _instances.Add(this);
       Id = _instances.Count;
+      Orders = new List<Order> { };
     }
 
     public static List<Vendor> GetAll()
