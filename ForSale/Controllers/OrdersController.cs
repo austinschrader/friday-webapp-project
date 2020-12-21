2dd3905340d1b6c929995dfb1a7e578bbc14aa2d
Austin Schrader <austin.jschrader@gmail.com>
2020-12-20 23:11:41 -0800

    Add orders controller base file

---
+++ ForSale/Controllers/OrdersController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using ForSale.Models;
+using System.Collections.Generic;
+
+namespace ForSale.Controllers
+{
+  public class OrdersController : Controller
+  {
+
+  }
+}
