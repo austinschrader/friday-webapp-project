b57d53e1afb61ee48fc106886f23b05333a4609c
Austin Schrader <austin.jschrader@gmail.com>
2020-12-20 23:15:00 -0800

    Add helper functions ClearAll and Find

--- ForSale/Models/Order.cs
+++ ForSale/Models/Order.cs
@@ -20,5 +20,15 @@ namespace ForSale.Models
       return _instances;
     }
 
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static Orders Find(int searchId)
+    {
+      return _instances[searchId - 1];
+    }
+
   }
 }
