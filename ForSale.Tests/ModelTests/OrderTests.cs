aee85a9a68404ec199093e712ba2b5a1532e1cad
Austin Schrader <austin.jschrader@gmail.com>
2020-12-22 18:03:18 -0800

    add order test for description

--- ForSale.Tests/ModelTests/OrderTests.cs
+++ ForSale.Tests/ModelTests/OrderTests.cs
@@ -16,13 +16,13 @@ namespace ForSale.Tests
       Assert.AreEqual(myOrder, newOrder.Title);
     }
 
-    // [TestMethod]
-    // public void OrderConstructor_CreatesInstanceOfOrder_OrderDescription()
-    // {
-    //   Order newOrder = new Order("Order One", "Pastries order", 50, "12/22/2020");
-    //   string myOrder = "Pastries order";
-    //   Assert.AreEqual(typeof(Order), newOrder.GetType());
-    //   Assert.AreEqual(myOrder, newOrder.Description);
-    // }
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_OrderDescription()
+    {
+      Order newOrder = new Order("Order One", "Pastries order", 50, "12/22/2020");
+      string myOrder = "Pastries order";
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+      Assert.AreEqual(myOrder, newOrder.Description);
+    }
   }
 }
