c24c63aaacdc6780e06c8a374294ec72bedd8d71
Austin Schrader <austin.jschrader@gmail.com>
2020-12-22 18:18:25 -0800

    Add vendor description test

--- ForSale.Tests/ModelTests/VendorTests.cs
+++ ForSale.Tests/ModelTests/VendorTests.cs
@@ -8,12 +8,22 @@ namespace ForSale.Tests
   {
 
     [TestMethod]
-    public void VendorConstructor_CreatesInstanceOfVendor_VendorTitle()
+    public void VendorConstructor_CreatesInstanceOfVendor_VendorName()
     {
       Vendor newVendor = new Vendor("Marie's Cafe", "Marie started her bakery in 1950.");
       string myVendor = "Marie's Cafe";
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
       Assert.AreEqual(myVendor, newVendor.Name);
+    }
+
+
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_VendorDescription()
+    {
+      Vendor newVendor = new Vendor("Marie's Cafe", "Marie started her bakery in 1950.");
+      string myVendor = "Marie started her bakery in 1950.";
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+      Assert.AreEqual(myVendor, newVendor.VendorDescription);
     }
   }
 }
