5bfade6f4ae53aa41d62a7bc12ecc656d44ebaf9
Austin Schrader <austin.jschrader@gmail.com>
2020-12-22 18:17:46 -0800

    Add vendor title test

--- ForSale.Tests/ModelTests/VendorTests.cs
+++ ForSale.Tests/ModelTests/VendorTests.cs
@@ -1,16 +1,19 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ForSale.Models;
 
-namespace Vendor.Tests
+namespace ForSale.Tests
 {
   [TestClass]
   public class VendorTest
   {
 
     [TestMethod]
-    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    public void VendorConstructor_CreatesInstanceOfVendor_VendorTitle()
     {
-
+      Vendor newVendor = new Vendor("Marie's Cafe", "Marie started her bakery in 1950.");
+      string myVendor = "Marie's Cafe";
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+      Assert.AreEqual(myVendor, newVendor.Name);
     }
   }
 }
