52396e09bf45bc95a0df39b6586286771f2ba250
Austin Schrader <austin.jschrader@gmail.com>
2020-12-22 18:04:18 -0800

    Add order price test

--- ForSale.Tests/ModelTests/OrderTests.cs
+++ ForSale.Tests/ModelTests/OrderTests.cs
@@ -24,5 +24,25 @@ namespace ForSale.Tests
       Assert.AreEqual(typeof(Order), newOrder.GetType());
       Assert.AreEqual(myOrder, newOrder.Description);
     }
+
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_OrderPrice()
+    {
+      Order newOrder = new Order("Order One", "Pastries order", 50, "12/22/2020");
+      int myOrder = 50;
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+      Assert.AreEqual(myOrder, newOrder.Price);
+    }
+
+    // [TestMethod]
+    // public void OrderConstructor_CreatesInstanceOfOrder_OrderDate()
+    // {
+    //   Order newOrder = new Order("Order One", "Pastries order", 50, "12/22/2020");
+    //   string myOrder = "12/22/2020";
+    //   Assert.AreEqual(typeof(Order), newOrder.GetType());
+    //   Assert.AreEqual(myOrder, newOrder.Date);
+    // }
+
+
   }
 }
